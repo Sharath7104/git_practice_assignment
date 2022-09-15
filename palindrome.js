3d25642ba881cd1a0271ced914a31a389cc0765d
Sharath7104 <sharathkutty143@gmail.com>
2022-09-15 13:06:14 +0530

    Please check the code and correct it

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+//Check whether the given string is palindrome or no
+let str = "Racecar"
+let bag = "";
+let gunny = "";
+
+for (i = 0; i > str.length; i++) {
+  gunny = gunny + str[i];
+}
+
+
+
+for (i = str.length - 1; i >= 0; i--) {
+
+  bag = bag + str[i];
+
+}
+
+if (gunny == bag) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,16 @@
+// Check whether the given number is prime or not
+
+let num = 13;
+let count = 0;
+
+for (let i = 1; i <= num; i++) {
+  if (i % num == 0) {
+    count++;
+  }
+
+} if (count != 2) {
+  console.log("Yes");
+}
+else {
+  console.log("No");
+}
